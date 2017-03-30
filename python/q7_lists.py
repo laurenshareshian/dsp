c7d5e2d381bcb71aee5105eda887c990f6f03351
laurenshareshian <laurenshareshian@gmail.com>
2017-03-29 20:43:42 -0700

    edited q7 python file

--- python/q7_lists.py
+++ python/q7_lists.py
@@ -7,14 +7,14 @@ def match_ends(words):
     Given a list of strings, return the count of the number of strings
     where the string length is 2 or more and the first and last chars
     of the string are the same.
-
-    >>> match_ends(['aba', 'xyz', 'aa', 'x', 'bbb'])
-    3
-    >>> match_ends(['', 'x', 'xy', 'xyx', 'xx'])
-    2
-    >>> match_ends(['aaa', 'be', 'abc', 'hello'])
-    1
     """
+    count = 0
+    for word in words:
+        if len(word) >= 2:
+            if word[0] == word[len(word)-1]:
+                count = count + 1
+    return count
+
     raise NotImplementedError
 
 
@@ -24,14 +24,20 @@ def front_x(words):
     order, except group all the strings that begin with 'x' first.
     e.g. ['mix', 'xyz', 'apple', 'xanadu', 'aardvark'] yields
          ['xanadu', 'xyz', 'aardvark', 'apple', 'mix'].
-
-    >>> front_x(['bbb', 'ccc', 'axx', 'xzz', 'xaa'])
-    ['xaa', 'xzz', 'axx', 'bbb', 'ccc']
-    >>> front_x(['ccc', 'bbb', 'aaa', 'xcc', 'xaa'])
-    ['xaa', 'xcc', 'aaa', 'bbb', 'ccc']
-    >>> front_x(['mix', 'xyz', 'apple', 'xanadu', 'aardvark'])
-    ['xanadu', 'xyz', 'aardvark', 'apple', 'mix']
     """
+    new_list=[]
+
+    words.sort()
+    for word in words:
+        if word[0]=='x':
+            new_list.append(word)
+
+    for word in words:
+        if word[0]!='x':
+            new_list.append(word)
+
+    return new_list
+
     raise NotImplementedError
 
 
@@ -41,14 +47,10 @@ def sort_last(tuples):
     increasing order by the last element in each tuple.
     e.g. [(1, 7), (1, 3), (3, 4, 5), (2, 2)] yields
          [(2, 2), (1, 3), (3, 4, 5), (1, 7)].
-
-    >>> sort_last([(1, 3), (3, 2), (2, 1)])
-    [(2, 1), (3, 2), (1, 3)]
-    >>> sort_last([(2, 3), (1, 2), (3, 1)])
-    [(3, 1), (1, 2), (2, 3)]
-    >>> sort_last([(1, 7), (1, 3), (3, 4, 5), (2, 2)])
-    [(2, 2), (1, 3), (3, 4, 5), (1, 7)]
     """
+    output = sorted(tuples, key = lambda x: x[-1])
+    return output
+
     raise NotImplementedError
 
 
@@ -58,16 +60,17 @@ def remove_adjacent(nums):
     elements have been reduced to a single element, so [1, 2, 2, 3]
     returns [1, 2, 3]. You may create a new list or modify the passed
     in list.
-
-    >>> remove_adjacent([1, 2, 2, 3])
-    [1, 2, 3]
-    >>> remove_adjacent([2, 2, 3, 3, 3])
-    [2, 3]
-    >>> remove_adjacent([3, 2, 3, 3, 3])
-    [3, 2, 3]
-    >>> remove_adjacent([])
-    []
     """
+    try:
+        new_list=[]
+        new_list.append(nums[0])
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i-1]:
+                new_list.append(nums[i])
+        return new_list
+    except:
+        return nums
+
     raise NotImplementedError
 
 
@@ -77,12 +80,36 @@ def linear_merge(list1, list2):
     merged list of all the elements in sorted order. You may modify
     the passed in lists. Ideally, the solution should work in "linear"
     time, making a single pass of both lists.
-
-    >>> linear_merge(['aa', 'xx', 'zz'], ['bb', 'cc'])
-    ['aa', 'bb', 'cc', 'xx', 'zz']
-    >>> linear_merge(['aa', 'xx'], ['bb', 'cc', 'zz'])
-    ['aa', 'bb', 'cc', 'xx', 'zz']
-    >>> linear_merge(['aa', 'aa'], ['aa', 'bb', 'bb'])
-    ['aa', 'aa', 'aa', 'bb', 'bb']
     """
+
+    new_list = []
+    count1 = 0
+    count2 = 0
+
+    for word in list1:
+        if count2!= len(list2): #if there are still words in the second list to check
+            if word <= list2[count2]:
+                new_list.append(word) #add the current word in the first list
+                count1 = count1 + 1
+
+            else:
+                while word > list2[count2]: #while the current word in the first list is after the words in the second list
+                    new_list.append(list2[count2])
+                    count2 = count2 + 1
+                    if count2 == len(list2): #if you have added all of the second list then stop
+                        break
+                new_list.append(word) #add the current word in the first list
+                count1 = count1 + 1
+
+    if count1 < len(list1): #if you haven't finished the first list then add them all at the end
+        for i in range(count1, len(list1)):
+            new_list.append(list1[i])
+
+    if count2 < len(list2):  #if you haven't finished the second list then add them all at the end
+        for i in range(count2, len(list2)):
+            new_list.append(list2[i])
+
+    return new_list
+
     raise NotImplementedError
+
